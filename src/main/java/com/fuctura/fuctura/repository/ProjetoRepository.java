ab413246061727234e9cbded1ea115c6a02a3157
Willyam Santos <consultor.willyam@leomadeiras.com.br>
2021-12-11 14:44:05 -0300

    feat: Create Repository Projeto

---
+++ src/main/java/com/fuctura/fuctura/repository/ProjetoRepository.java
@@ -0,0 +1,12 @@
+package com.fuctura.fuctura.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fuctura.fuctura.model.Projeto;
+
+public interface ProjetoRepository extends JpaRepository<Projeto,Long>{
+  List<Projeto> findByPublished(boolean published);
+  List<Projeto> findByNameContains(String name);
+}
